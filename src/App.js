cf04b678d0d1439d3d7243fdceeab2de881a517a
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-04-11 18:03:02 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Quiz = () => {
+  const [questions, setQuestions] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple')
+      .then(response => response.json())
+      .then(data => {
+        setQuestions(data.results);
+        console.log(data.results);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleAnswer = (questionIndex, answer) => {
+    console.log(answer);
+    setSelectedAnswer(prevState => {
+      const newState = [...prevState];
+      newState[questionIndex] = answer;
+      return newState;
+    });
+  }
+
+  const getAnswerStyle = (answer) => {
+    if (answer === questions.find(q => q.correct_answer === selectedAnswer)?.correct_answer) {
+      return { backgroundColor: 'green' };
+    } else if (answer === selectedAnswer) {
+      return { backgroundColor: 'red' };
+    } else {
+      return {};
+    }
+  }
+
+  const isQuestionAnswered = (questionIndex) => {
+    return selectedAnswer[questionIndex] !== undefined;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Quiz App</h2>
+      <h4>General Knowledge</h4>
+      <hr />
+      {questions.map((question, index) => (
+        <div key={index}>
+          <p>{question.question}</p>
+          <ul>
+            {question.incorrect_answers.map((answer, index) => (
+              <li key={index}>
+                <button style={getAnswerStyle(answer)} disabled={isQuestionAnswered(index)} onClick={() => handleAnswer(index, answer)}>{answer}</button>
+              </li>
+            ))}
+            <li key="correct">
+              <button style={getAnswerStyle(question.correct_answer)} disabled={isQuestionAnswered(index)} onClick={() => handleAnswer(index, question.correct_answer)}>{question.correct_answer}</button>
+            </li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default Quiz;
